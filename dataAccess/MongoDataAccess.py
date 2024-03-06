f5afd177e36fe547b774293e45b3d8866668733f
mattespositotech <matt@teamduality.dev>
2024-03-06 08:59:46 -0600

    Try Execept to data access

--- dataAccess/MongoDataAccess.py
+++ dataAccess/MongoDataAccess.py
@@ -4,43 +4,72 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 from utils.constants import DATABASE_NAME
 
-# add try catch blocks to all functions
 class MongoDataAccess:
     def __init__(self, collection):
-        load_dotenv()
-        MONGODB_URI = os.environ['MONGODB_URI']
-        self.client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
+        try:
+            load_dotenv()
+            MONGODB_URI = os.environ['MONGODB_URI']
+            self.client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
 
-        database = DATABASE_NAME
-        self.cursor = self.client[database]
-        self.collection = self.cursor[collection]
+            database = DATABASE_NAME
+            self.cursor = self.client[database]
+            self.collection = self.cursor[collection]
+        except Exception as e:
+            print("An error occurred during initialization:", e)
     
     def change_collection(self, collection):
-        self.collection = self.cursor[collection]
+        try:
+            self.collection = self.cursor[collection]
+        except Exception as e:
+            print("An error occurred while changing collection:", e)
     
     def read_one(self, query):
-        document = self.collection.find_one(query)
-        if (document):
-            document['_id'] = str(document['_id'])
-            return document
-        else:
+        try:
+            document = self.collection.find_one(query)
+            if document:
+                document['_id'] = str(document['_id'])
+                return document
+            else:
+                return None
+        except Exception as e:
+            print("An error occurred while reading one document:", e)
             return None
     
     def read_all(self, query):
-        documents = self.collection.find(query)
-        return [{**doc, '_id': str(doc['_id'])} for doc in documents]
+        try:
+            documents = self.collection.find(query)
+            return [{**doc, '_id': str(doc['_id'])} for doc in documents]
+        except Exception as e:
+            print("An error occurred while reading all documents:", e)
+            return []
     
     def insert_one(self, record):
-        inserted = self.collection.insert_one(record)
-        return str(inserted.inserted_id)
+        try:
+            inserted = self.collection.insert_one(record)
+            return str(inserted.inserted_id)
+        except Exception as e:
+            print("An error occurred while inserting one document:", e)
+            return None
     
     def update_one(self, query, update):
-        updated = self.collection.update_one(query, update)
-        return updated.modified_count
+        try:
+            updated = self.collection.update_one(query, update)
+            return updated.modified_count
+        except Exception as e:
+            print("An error occurred while updating one document:", e)
+            return 0
     
     def delete_one(self, query):
-        deleted = self.collection.delete_one(query)
-        return deleted.deleted_count
+        try:
+            deleted = self.collection.delete_one(query)
+            return deleted.deleted_count
+        except Exception as e:
+            print("An error occurred while deleting one document:", e)
+            return 0
     
     def aggregate_as_list(self, pipeline):
-        return list(self.collection.aggregate(pipeline))
+        try:
+            return list(self.collection.aggregate(pipeline))
+        except Exception as e:
+            print("An error occurred while aggregating:", e)
+            return []
