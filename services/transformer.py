b02cd4f026dd0eeb4e22d008fd79cfab061d5aaf
mattespositotech <matt@teamduality.dev>
2024-08-17 16:19:35 -0500

    Removed unused import and comment

--- services/transformer.py
+++ services/transformer.py
@@ -1,4 +1,3 @@
-import json
 from enums.roundup_enums import RoundupStatus
 from enums.user_enums import UserStatus
 
@@ -14,8 +13,6 @@ def prep_roundup_for_mongo(roundup):
     roundup['blacklists'] = remove_duplicates(roundup['blacklists'])
 
     return roundup
-
-    #roundup_json = json.dumps(roundup)
 
 def remove_duplicates(blacklist):
     seen = set()
