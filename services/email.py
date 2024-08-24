64977e8d3b5bbc8ab37abbe0a070d0992c750de6
mattespositotech <matt@teamduality.dev>
2024-08-24 12:08:10 -0500

    Email Cleanup

--- services/email.py
+++ services/email.py
@@ -5,7 +5,9 @@ import smtplib
 from email.mime.text import MIMEText
 import services.html_builder as hb
 
-def send_email(subject, body, recipient, image_path):
+def send_email(subject, body, recipient):
+    logo_path = os.path.join(os.path.dirname(__file__), '../templates/Raindeer Roundup.png')
+
     sender = os.environ['EMAIL_ID']
     password = os.environ['EMAIL_PASSWORD']
 
@@ -17,25 +19,15 @@ def send_email(subject, body, recipient, image_path):
 
     msg.attach(MIMEText(body, 'html'))
 
-    with open(image_path, 'rb') as img_file:
-        image = MIMEImage(img_file.read())
-        image.add_header('Content-ID', '<reindeer-logo>')
-        msg.attach(image)
+    with open(logo_path, 'rb') as img_file:
+        logo = MIMEImage(img_file.read())
+        logo.add_header('Content-ID', '<reindeer-logo>')
+        msg.attach(logo)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
        smtp_server.login(sender, password)
        smtp_server.sendmail(sender, recipient, msg.as_string())
     print("Message sent!")
-
-# def test_email():
-#     subject = "You've Been Invited To A Reindeer Roundup! 5"
-#     rep = os.environ['TEST_RECIPIENT']
-#     recipients = [rep]
-
-#     body = hb.reciever_builder('thisIsTheRoundUpId', participant)
-
-#     image_path = os.path.join(os.path.dirname(__file__), '../templates/Raindeer Roundup.png')
-#     send_email(subject, body, recipients, image_path)
 
 def send_invites(roundup):
     subject = "You've Been Invited To A Reindeer Roundup! 7"
@@ -45,11 +37,10 @@ def send_invites(roundup):
         'date': roundup['date'],
         'message': roundup['message']
     }
-    image_path = os.path.join(os.path.dirname(__file__), '../templates/Raindeer Roundup.png')
 
     for part in roundup['participants']:
         if '@test.com' not in part['email']:
             body = hb.invitation_builder(roundupEmailInfo, part['email'])
-            send_email(subject, body, part['email'], image_path)
+            send_email(subject, body, part['email'])
         else:
             print('skip')
