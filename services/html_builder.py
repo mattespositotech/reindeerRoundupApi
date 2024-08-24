fab26a6d6531a413911e341a4866790d0c60d05d
mattespositotech <matt@teamduality.dev>
2024-08-24 12:05:09 -0500

    Refactored html_builder

--- services/html_builder.py
+++ services/html_builder.py
@@ -1,55 +1,55 @@
 import os
 from bs4 import BeautifulSoup
 
-def invitation_builder(roundup, email):
-    roundupId = roundup['id']
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), '../templates')
 
-    template_path = os.path.join(os.path.dirname(__file__), '../templates/invitation_template.html')
-    with open(template_path, 'r') as file:
-        template = file.read()
+def load_template(template_name):
+    template_path = os.path.join(TEMPLATE_DIR, template_name)
+    try:
+        with open(template_path, 'r') as file:
+            return BeautifulSoup(file.read(), 'html.parser')
+    except FileNotFoundError:
+        raise Exception(f"Template {template_name} not found.")
 
-    soup = BeautifulSoup(template, 'html.parser')
-
-    roundupData = {
-        'title': roundup['title'],
-        'date': roundup['date'],
-        'message': roundup['message']
-    }
-
-    for id, new_text in roundupData.items():
+def update_template(soup, data):
+    for id, new_text in data.items():
         element = soup.find(id=id)
         if element:
             element.string = new_text
 
-    participantButtons = {
-        'accept': f"/{roundupId}/{email}",
-        'decline': f"/{roundupId}/{email}"
-    }
-
-    for id, link in participantButtons.items():
+def update_links(soup, links):
+    for id, link in links.items():
         element = soup.find(id=id)
         if element:
             element['href'] += link
-    
+
+def invitation_builder(roundup, email):
+    soup = load_template('invitation_template.html')
+
+    roundup_data = {
+        'title': roundup['title'],
+        'date': roundup['date'],
+        'message': roundup['message']
+    }
+    update_template(soup, roundup_data)
+
+    participant_buttons = {
+        'accept': f"/{roundup['id']}/{email}",
+        'decline': f"/{roundup['id']}/{email}"
+    }
+    update_links(soup, participant_buttons)
+
     return str(soup)
 
-def reciever_builder():
-    template_path = os.path.join(os.path.dirname(__file__), '../templates/reciever_template.html')
-    with open(template_path, 'r') as file:
-        template = file.read()
+def receiver_builder():
+    soup = load_template('receiver_template.html')
 
-    soup = BeautifulSoup(template, 'html.parser')
-
-    data = {
+    receiver_data = {
         'user-name': 'Billy',
         'org-name': 'Matt',
         'date': '12-25-2024',
         'receiver-name': 'Jack'
     }
+    update_template(soup, receiver_data)
 
-    for id, new_text in data.items():
-        element = soup.find(id=id)
-        if element:
-            element.string = new_text
-    
     return str(soup)
